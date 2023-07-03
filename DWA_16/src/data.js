96aabddfec44ade4ea6e1077969ac4c2b9bca9be
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-03 15:40:21 +0200

    Add card carousel css.

--- DWA_16/src/data.js
+++ DWA_16/src/data.js
@@ -37,5 +37,44 @@ export default [
         },
         location: "Norway",
         openSpots: 3,
-    }
+    },
+    {
+        id: 4,
+        title: "Two Oceans Aquarium Tour",
+        description: "Visit the world renowned Two Oceans Aquarium. See penguins, stingray sharks and take a deep sea diving lesson. Enjoy marine life like you've never before!",
+        price: 22,
+        coverImg: "https://cdn.pixabay.com/photo/2017/08/03/10/59/nature-2575575_1280.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 225
+        },
+        location: "South Africa",
+        openSpots: 10,
+    },
+    {
+        id: 5,
+        title: "Sudwala Caves Expedition",
+        description: "Uncover the hidden wonders of Mpumalanga, South Africa, with our Sudwala Caves tour. Delve deep into the ancient caverns adorned with stunning stalactites and stalagmites. Experience the thrill of abseiling down underground cliffs or find serenity in the Garden of Eden within the caves. Don't miss the nearby Sudwala Dinosaur Park for a journey back in time.",
+        price: 115,
+        coverImg: "https://cdn.pixabay.com/photo/2017/01/21/21/31/drach-1998338_640.jpg",
+        stats: {
+            rating: 4.7,
+            reviewCount: 59,
+        },
+        location: "South Africa",
+        openSpots: 8,
+    },
+    {
+        id: 6,
+        title: "Baba Yega's Storytime",
+        description: "Join us for an enchanting evening of whimsy and wonder as we embark on a magical journey through the realm of fairy tales. Immerse yourself in a world of mythical creatures, brave heroes, and wicked villains, as seasoned storytellers transport you to faraway lands filled with spellbinding adventures. Prepare to be captivated by the age-old tales that have captured the hearts of generations and let your imagination take flight at this extraordinary fairy story telling event.",
+        price: 12,
+        coverImg: "https://cdn.pixabay.com/photo/2022/09/19/20/40/house-7466438_1280.jpg",
+        stats: {
+            rating: 2.1,
+            reviewCount: 510,
+        },
+        location: "Online",
+        openSpots: 89,
+    },
 ]
