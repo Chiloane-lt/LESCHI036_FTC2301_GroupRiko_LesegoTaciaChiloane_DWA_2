649ab3dc1e937f08204cfc7202ac555e99548385
Chiloane-lt <chiloane.lt@hotmail.com>
2023-07-06 10:58:36 +0200

    Update meme container.

--- DWA_17/src/components/Content.jsx
+++ DWA_17/src/components/Content.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+
+import Data from '../memesData';
 
 export default function Content() {
+
+  const [ meme, setMeme ] = useState("");
+
+  function getMeme() {
+    const randomIndex = Math.floor(Math.random()*100);
+    const newmeme = Data.data.memes[randomIndex];
+    const { url } = newmeme;
+    return url;
+}
+
+  function clickHandler() {
+    setMeme(getMeme());
+  }
+
   return (
     <div className="ContentGenerator">
       <div className="generator">
         <input type="text" className="generator--input input-top" />
         <input type="text" className="generator--input input--bottom"/>
-        <button className="generator--button">
+        <button className="generator--button" onClick={clickHandler}>
           Get New Meme Image
-          <span> <i class="fa-sharp fa-solid fa-image"></i></span>
+          <span> <i className="fa-sharp fa-solid fa-image"></i></span>
         </button>
       </div>
-      <div className="meme-container">Meme Image Goes Here!</div>
+      <div className="meme-container">
+        <img src={meme} className="meme-container-image"/>
+      </div>
     </div>
   )
 }
